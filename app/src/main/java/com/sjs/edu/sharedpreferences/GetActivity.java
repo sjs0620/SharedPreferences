b5e6b53dbbf796646474d63e827c7e24a90106b0
신재순 <sjslsm@gmail.com>
2018-10-31 16:26:06 +0900

    second

--- app/src/main/java/com/sjs/edu/sharedpreferences/GetActivity.java
+++ app/src/main/java/com/sjs/edu/sharedpreferences/GetActivity.java
@@ -1,13 +1,42 @@
 package com.sjs.edu.sharedpreferences;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class GetActivity extends AppCompatActivity {
+public class GetActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get);
+        ((Button)findViewById(R.id.buttonGet)).setOnClickListener(this);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("LogIn",Context.MODE_PRIVATE);
+        String nameKey = sharedPreferences.getString("nameKey","Null");
+        String passwordKey = sharedPreferences.getString("passwordKey","Null");
+        String emailKey = sharedPreferences.getString("emailKey","Null");
+
+        ((TextView)findViewById(R.id.textViewGetName)).setText(nameKey);
+        ((TextView)findViewById(R.id.textViewGetPassword)).setText(passwordKey);
+        ((TextView)findViewById(R.id.textViewGetMail)).setText(emailKey);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this,MainActivity.class);
+        Bundle bundle = new Bundle();
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
     }
 }
--- app/src/main/java/com/sjs/edu/sharedpreferences/InputActivity.java
+++ app/src/main/java/com/sjs/edu/sharedpreferences/InputActivity.java
@@ -1,13 +1,46 @@
 package com.sjs.edu.sharedpreferences;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class InputActivity extends AppCompatActivity {
+public class InputActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+        Button button = findViewById(R.id.buttonInputSave);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        SharedPreferences.Editor editor = getSharedPreferences("LogIn",Context.MODE_PRIVATE).edit();
+        editor.putString("nameKey",((EditText)findViewById(R.id.editTextName)).getText().toString());
+        editor.putString("passwordKey",((EditText)findViewById(R.id.editTextPassword)).getText().toString());
+        editor.putString("emailKey",((EditText)findViewById(R.id.editTextMail)).getText().toString());
+        editor.commit();
+
+        Intent intent = new Intent(this,GetActivity.class);
+        Bundle bundle = new Bundle();
+        intent.putExtras(bundle);
+        startActivityForResult(intent,10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
     }
 }
